294498553a3e9388a710ccd2f0493556382f5d98
VGanev <siwd@abv.bg>
2026-09-25 10:39:58 +0300

    all

--- WebApplicationRazor/WebApplicationRazor/obj/Debug/net10.0/WebApplicationRazor.AssemblyInfo.cs
+++ WebApplicationRazor/WebApplicationRazor/obj/Debug/net10.0/WebApplicationRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9683ee3c5df5c4dfb53c0aa316f87824968e9815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf23eec084633ab3796b63db26fdd29a076549b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
